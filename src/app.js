5b939a841999e30767d017e504024ab4cab38af6
Priya Sharma <92673866+priyaaa404@users.noreply.github.com>
2026-02-28 19:15:56 +0530

    posted data intodb

--- src/app.js
+++ src/app.js
@@ -4,18 +4,56 @@ const app = express();
 const User = require("./models/user.js");
 
 
-app.post("/signUp", async (req,res) => {
-    const user = new User({
-        firstName : "priya", 
-        lastName : "sharma",
-        emailId : "sharma.priya162002@gmail.com",
-        password : "priya123"
-    });
 
-    await user.save();
-    res.send("User added succesfully");
-});
+// app.post("/signUp", async (req,res) => {
+//     const user = new User({
+//         firstName : "priya", 
+//         lastName : "sharma",
+//         emailId : "sharma.priya162002@gmail.com",
+//         password : "priya123"
+//     });
+
+//     await user.save();
+//     res.send("User added succesfully");
+// });
  
+app.use(express.json());
+
+app.post("/signUp", async (req,res) => {
+    const user = new User(req.body);
+ 
+    try{
+        await user.save();
+        res.send("user added succesfully");
+    } catch (err){
+        res.status(400).send("something went wrong" + err.message);
+    }
+    res.send("data printed")
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 connectDB()
 .then(() => {   
     console.log("db success")
