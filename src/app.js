830762a6a8a16df936203891a64cef28df87eba1
Priya Sharma <92673866+priyaaa404@users.noreply.github.com>
2026-03-02 12:26:00 +0530

    data sanitization and password encryption

--- src/app.js
+++ src/app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const connectDB = require("./config/database.js");
 const app = express();
 const User = require("./models/user.js");
-
-
+const {validateSignUpData} = require("./utils/validation");
+const bcrypt = require("bcrypt");
+const user = require("./models/user.js");
 
 // app.post("/signUp", async (req,res) => {
 //     const user = new User({
@@ -19,23 +20,97 @@ const User = require("./models/user.js");
  
 app.use(express.json());
 
+//posts user in db
 app.post("/signUp", async (req,res) => {
-    const user = new User(req.body);
- 
     try{
+
+        validateSignUpData(req);
+
+        const { firstName, lastName, emailId, password } = req.body;
+
+        const passwordHash = await bcrypt.hash(password , 10);
+
+        const user = new User({     
+        firstName,
+        lastName,
+        emailId,
+        password : passwordHash,
+        });
+
         await user.save();
         res.send("user added succesfully");
+
     } catch (err){
-        res.status(400).send("something went wrong" + err.message);
+        res.status(400).send("ERROR" + err.message);
     }
-    res.send("data printed")
 });
 
+app.post("/login", async (req,res) => {
+    try{
+        const {emailId,password} = req.body;
+
+        const user = await User.findOne({emailId : emailId});
+        if(!user){
+            throw new Error("Invalid credentials");
+        }
+
+        const isPasswordValid = await bcrypt.compare(password,user.password);
+        if(isPasswordValid){
+            res.send("Login succesfully");
+        } else {
+            throw new Error("Invalid credentials");        }
+
+    } catch (err){
+        res.status(400).send("ERROR" + err.message);
+    }
+})
 
 
+// finds user by email
+app.get("/user", async (req,res) => {
+    const userEmailId = req.body.emailId;
 
+    try{
+     const user = await User.find( { emailId : userEmailId});
+     res.send(user);
+    } catch (err){
+        res.status(400).send("cannot find user");
+    }
+    
+});
+// gets all users
+app.get('/feed', async (req,res) => {
+    try{
+        const user = await User.find({});
+        res.send(user); 
+    } catch (err) {
+        res.status(400).send("cannot find user");
+    }
+});
 
+app.delete('/user', async (req,res) => {
+    
+    const userId  =  req.body.userId;
 
+    try{
+        const user = await User.findByIdAndDelete(userId);
+        res.send("user deleted succesfully"); 
+    } catch (err) {
+        res.status(400).send("cannot find user");
+    }
+});
+
+app.patch('/user', async (req,res) => {
+    const userId = req.body.userId;
+    const data  =  req.body;
+
+    try{
+        await User.findByIdAndUpdate({_id : userId}, data);
+        res.send("user updated succesfully"); 
+    } catch (err) {
+        res.status(400).send("cannot find user");
+    }
+});
 
 
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,48 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
 
 const userSchema = mongoose.Schema({
     firstName : {
-        type : "string"
+        type : String , 
+        required : true ,
+        trim : true ,
+        minLength : 2
     }, 
     lastName  : {
-        type : "string"
+        type : String ,
+        required : true ,
+        trim : true ,
+        minLength : 2
     }, 
     emailId : {
-        type : "string"
+        type : String ,
+        required : true ,
+        unique : true,
+        trim : true ,
+        // validate(value){
+        //     if(!validator.isEmail(value)){
+        //         throw new Error ("Invalid email id" + value)
+        //     }
+        // }
     },
     password  : {
-        type : "string"
+        type : String ,
+        required : true ,
+        trim : true
     },
     age  : {
-        type : Number
+        type : Number ,
+        trim : true ,
+    
     },
     gender  : {
-        type : "string"
+        type : String ,
+        // required : true ,
+        trim : true 
     }
+},
+{
+    timestamps : true
 });
 
 module.exports = mongoose.model("User", userSchema);
