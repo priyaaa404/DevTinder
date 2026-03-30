bfde5e6e19f18adb4b6b05cda9538fcfd548806e
Priya Sharma <92673866+priyaaa404@users.noreply.github.com>
2026-03-31 02:29:55 +0530

    Cors fixed

--- src/app.js
+++ src/app.js
@@ -11,7 +11,8 @@ require("dotenv").config();
 const app = express();
 
 app.use(cors({
-    origin : "http://localhost:5173" ,
+    origin : ["http://localhost:5173" ,
+    "https://dev-tinder-app-xi.vercel.app"],
     credentials : true ,
 })); 
 app.use(express.json());
